e45f3d3083b440e6de56ddbc647b330d20a2b10f
agonzaleztourn <82126569+agonzaleztourn@users.noreply.github.com>
2021-05-06 22:55:50 -0300

    Update EjemploTipos.cpp

--- 01-EjemploTipos/EjemploTipos.cpp
+++ 01-EjemploTipos/EjemploTipos.cpp
@@ -1,22 +1,20 @@
-#include <iostream>
 #include <cassert>
+#include <string>
 
 int main(){
+    // Ejemplo booleanos
     assert(true==true);
     assert(true!=false);
     assert(true or false==true);
     assert(true and true==true);
     assert(!false);
     assert(true xor false);
+    // Ejemplos char
     assert('a'=='a');
     assert('a'!='b');
-    assert('3'<='4');
-    assert('4'>='4');
     assert('A'+32=='a');
-    assert('a'-32=='A');
-    assert('3'*'2'==2550);
-    assert('2'/'2'==1);
-    assert('2'%'2'==0);
+    assert('a'-32=='A');    
+    //Ejemplos int
     assert(455==455);
     assert(55!=690);
     assert(56<=75);
@@ -26,20 +24,19 @@ int main(){
     assert(25*2==50);
     assert(50/2==25);
     assert(15%3==0);
+    //Ejemplos double
     assert(2.4-0.3==2.1);
     assert(2.4==2.4);
     assert(2.4!=6.3);
     assert(3.1>=2.6);
     assert(6.8<=7.9);
     assert(3.5*1.5==5.25);
-    std::string ejemplo="hola";
-    std::string ejemplo2="mundo";
-    assert(ejemplo.length()==4);
-    assert(ejemplo + ejemplo2=="holamundo");
-    assert(ejemplo!=ejemplo2);
-    assert(ejemplo==ejemplo);
-    assert(ejemplo<=ejemplo2);
-    assert(ejemplo2>=ejemplo);
-      
+    //Ejemplos string
+    assert(std::string("hola").length()==4);
+    assert(std::string("hola") + std::string("mundo")=="holamundo");
+    assert(std::string("casa")==std::string("casa"));
+    //Ejemplos unsigned
+    assert(2U+3U==5U);
+    assert(4U-3U==1U);  
 
 }
